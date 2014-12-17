5602a506b3e9ae2e1103983dbdfe8f9c97bfac24
waldenpond <yongbaolinux@gmail.com>
2014-12-18 01:34:26 +0800

    123

--- bui/main/env.php
+++ bui/main/env.php
@@ -1,3 +1,4 @@
+<?php @ini_set('short_open_tag' , 'On');?>
 <!DOCTYPE HTML>
 <html>
  <head>
@@ -23,17 +24,50 @@
              <h3>服务器信息</h3>
              <div class="row detail-row">
                  <div class="span8">
-                     <label>服务器IP：</label><span class="detail-text"><?php echo 1;?></span>
+                     <label>服务器IP：</label><span class="detail-text"><?=$_SERVER['SERVER_ADDR']?></span>
                  </div>
                  <div class="span8">
-                     <label>年龄：</label><span class="detail-text">21</span>
+                     <label>服务器主机名：</label><span class="detail-text"><?=$_SERVER['SERVER_NAME']?></span>
                  </div>
                  <div class="span8">
-                     <label>家庭住址：</label><span class="detail-text">速度发撒旦法师打法是否撒反对撒范德萨发撒旦法倒萨发生</span>
+                     <label>服务器CGI规范：</label><span class="detail-text"><?=$_SERVER['GATEWAY_INTERFACE']?></span>
                  </div>
              </div>
              <div class="row detail-row">
-
+                 <div class="span8">
+                     <label>服务器通信协议：</label><span class="detail-text"><?=$_SERVER['SERVER_PROTOCOL']?></span>
+                 </div>
+                 <div class="span8">
+                     <label>服务器管理员邮箱：</label><span class="detail-text"><?=$_SERVER['SERVER_ADMIN']?></span>
+                 </div>
+                 <div class="span8">
+                     <label>服务器Web端口：</label><span class="detail-text"><?=$_SERVER['SERVER_PORT']?></span>
+                 </div>
+             </div>
+             <div class="row detail-row">
+                 <div class="span8">
+                     <label>当前脚本url相对路径：</label><span class="detail-text"><?=$_SERVER['SCRIPT_NAME']?></span>
+                 </div>
+                 <div class="span8">
+                     <label>当前脚本磁盘路径：</label><span class="detail-text"><?=__FILE__?></span>
+                 </div>
+                 <!--<div class="span8">
+                     <label>当前脚本绝对路径：</label><span class="detail-text"><?/*=$_SERVER['SCRIPT_FILENAME']*/?></span>
+                 </div>-->
+                 <div class="span8">
+                     <label>服务器标识字符串：</label><span class="detail-text"><?=$_SERVER['SERVER_SOFTWARE']?></span>
+                 </div>
+             </div>
+             <div class="row detail-row">
+                 <div class="span8">
+                     <label>Apache服务器加载模块：</label><span class="detail-text"><?php foreach(apache_get_modules() as $key=>$module){
+                             if($key%2 == 1){
+                                 echo "<a href='http://httpd.apache.org/docs/2.4/mod/".$module.".html' target='_new' style='background-color:#ccc;'>".$module."</a> ";
+                             } else {
+                                 echo "<a href='http://httpd.apache.org/docs/2.4/mod/".$module.".html' target='_new'>".$module."</a> ";
+                             }
+                         }?></span>
+                 </div>
              </div>
          </div>
          <div class="detail-section">
