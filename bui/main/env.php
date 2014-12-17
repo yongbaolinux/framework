4d0692fefca468ddefafffdab6180c1cb265f68b
waldenpond <yongbaolinux@gmail.com>
2014-12-18 00:30:05 +0800

    123

---
+++ bui/main/env.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <title> 首页代码结构</title>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+       <link href="../assets/css/dpl-min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/css/bui-min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/css/page-min.css" rel="stylesheet" type="text/css" />   <!-- 下面的样式，仅是为了显示代码，而不应该在项目中使用-->
+   <style type="text/css">
+    code {
+      padding: 0px 4px;
+      color: #dd1144;
+      background-color: #f7f7f9;
+      border: 1px solid #e1e1e8;
+    }
+   </style>
+ </head>
+ <body>
+ <div class="container">
+     <div class="detail-page">
+         <h2>信息</h2>
+         <div class="detail-section">
+             <h3>服务器信息</h3>
+             <div class="row detail-row">
+                 <div class="span8">
+                     <label>服务器IP：</label><span class="detail-text"><?php echo 1;?></span>
+                 </div>
+                 <div class="span8">
+                     <label>年龄：</label><span class="detail-text">21</span>
+                 </div>
+                 <div class="span8">
+                     <label>家庭住址：</label><span class="detail-text">速度发撒旦法师打法是否撒反对撒范德萨发撒旦法倒萨发生</span>
+                 </div>
+             </div>
+             <div class="row detail-row">
+
+             </div>
+         </div>
+         <div class="detail-section">
+
+         </div>
+         <div class="detail-section">
+             
+         </div>
+     </div>
+ </div>
+  <script type="text/javascript" src="../assets/js/jquery-1.8.1.min.js"></script>
+  <script type="text/javascript" src="../assets/js/bui-min.js"></script>
+  <script type="text/javascript" src="../assets/js/config-min.js"></script>
+  <script type="text/javascript">
+    BUI.use('common/page');
+  </script>
+
+<body>
+</html>
