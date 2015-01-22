9115ac4072ff6159aa550d12733f1330a39a0101
yongbaolinux <yongbaolinux@gmail.com>
2015-01-22 22:40:25 +0800

    123

--- bui/BeyondCms/admin/controllers/config.php
+++ bui/BeyondCms/admin/controllers/config.php
@@ -7,8 +7,12 @@ class config extends CI_Controller{
     public function __construct(){
         parent::__construct();
         $this->load->database();
+        $this->load->helper('url
+            
+            ');
         $this->load->helper('common_func');
         $this->load->library('session_');
+        $this->admin_res = base_url().'admin_res/';
         if(!$this->session_->getSession('admin.login')){
             redirect('admin/login','location');
         }
@@ -18,8 +22,9 @@ class config extends CI_Controller{
      * 基础配置
      */
     public function baseConfig(){
+        $config = json_encode($this->db->query("SELECT * FROM `bd_config`")->result_array());
         
-        $this->load->view('baseConfig');
+        $this->load->view('baseConfig',array('config'=>$config,'PUBLIC'=>$this->admin_res));
     }
 }
 ?>
--- bui/BeyondCms/admin/views/baseConfig.php
+++ bui/BeyondCms/admin/views/baseConfig.php
@@ -3,8 +3,43 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>基础配置</title>
+<script type="text/javascript" src="<?=$PUBLIC?>/js/jquery-1.8.1.min.js"></script>
+<!-- 引入BUI -->
+<script type="text/javascript" src="<?=$PUBLIC?>/js/bui-min.js"></script>
+<script type="text/javascript" src="<?=$PUBLIC?>/js/config-min.js"></script>
 </head>
 
 <body>
 </body>
+<script type="text/javascript">
+	//渲染配置列表数据
+	BUI.use(['bui/grid'],function(Grid){
+		var columns = [ {title : '<input type="checkbox" onClick="javascript:chooseAllArticles(this)"/>',dataIndex :'', width:'2%',renderer:function(value,obj){
+							return '<input type="checkbox"  class="bui-grid-cell-text-checkbox" value="'+obj.id+'"/>';
+						}},
+						{title : 'id',dataIndex :'id', width:'10%'},
+			       		{title : '配置项英文名',dataIndex :'title', width:'20%'},
+			       		{title : '配置项中文名',dataIndex : 'cate_cname',width:'20%'},
+			       		{title : '配置项值',dataIndex :'ctime', width:'20%',renderer:BUI.Grid.Format.datetimeRenderer},
+			       		{title : '操作',dataIndex :'top', width:'10%',
+				       		renderer:function(value,obj){
+					       		 if(value === '1'){
+						       		   return '<a onClick="javascript:topArticle('+obj.id+',0,this)" href="javascript:void(0)">取消置顶</a> <a href="javascript:editArticle('+obj.id+')">编辑</a> <a href="javascript:delArticle('+obj.id+')">删除</a>';
+					       		 } else {
+						       			return '<a onClick="javascript:topArticle('+obj.id+',1,this)" href="javascript:void(0)">置顶</a> <a href="javascript:editArticle('+obj.id+')">编辑</a> <a href="javascript:delArticle('+obj.id+')">删除</a>';
+					       		 }
+					       	}
+			       		}];
+		var data = <?=$config?>;
+		for(i in data){
+			data[i]['ctime'] = parseInt(data[i]['ctime'])*1000;//BUI.Grid.Format.datetimeRenderer的参数为整形 所以在这里需要对其进行强制类型转换 （并且要注意 是毫秒数）
+		}
+		 var grid = new Grid.SimpleGrid({
+		  render:'#grid',
+		  columns : columns,
+		  items : data,
+		 });
+ 		 grid.render();
+	});
+</script>
 </html>
