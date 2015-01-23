63e898d403e4d397cd17c9fba6d633d157999793
yongbaolinux <yongbaolinux@gmail.com>
2015-01-23 17:58:10 +0800

    123

--- bui/BeyondCms/admin/controllers/config.php
+++ bui/BeyondCms/admin/controllers/config.php
@@ -7,19 +7,10 @@ class config extends CI_Controller{
     public function __construct(){
         parent::__construct();
         $this->load->database();
-<<<<<<< HEAD
-        $this->load->helper('url
-            
-            ');
-        $this->load->helper('common_func');
-        $this->load->library('session_');
-        $this->admin_res = base_url().'admin_res/';
-=======
         $this->load->helper('url');
         $this->load->helper('common_func');
         $this->load->library('session_');
         $this->admin_res = base_url().'admin_res/';                 //后台资源存放路径(js css images)
->>>>>>> 33149da79db35b76c79e3b317008f20c67a4a382
         if(!$this->session_->getSession('admin.login')){
             redirect('admin/login','location');
         }
@@ -29,16 +20,20 @@ class config extends CI_Controller{
      * 基础配置
      */
     public function baseConfig(){
-<<<<<<< HEAD
         $config = json_encode($this->db->query("SELECT * FROM `bd_config`")->result_array());
-        
         $this->load->view('baseConfig',array('config'=>$config,'PUBLIC'=>$this->admin_res));
-=======
-        $configResource = $this->db->query("SELECT * FROM `bd_config`");
-        $config = json_encode($configResource->result_array());
-        
-        $this->load->view('baseConfig',array('CONFIG'=>$config,'PUBLIC'=>$this->admin_res));
->>>>>>> 33149da79db35b76c79e3b317008f20c67a4a382
+    }
+    
+    /**
+     * ajax方式删除配置项
+     * 一次删除一个和多个配置项均使用该方法
+     */
+    public function ajaxDelConfigs(){
+        if(isAjax()){
+            
+        } else {
+            exit(json_encode(array('code'=>-100,'msg'=>'非法访问')));
+        }
     }
 }
 ?>
--- bui/BeyondCms/admin/views/baseConfig.php
+++ bui/BeyondCms/admin/views/baseConfig.php
@@ -3,14 +3,17 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>基础配置</title>
+<link href="<?=$PUBLIC?>/css/dpl-min.css" rel="stylesheet" type="text/css" />
+<link href="<?=$PUBLIC?>/css/bui-min.css" rel="stylesheet" type="text/css" /
+</head>
+
+<body>
+	<div id="grid" style="margin:20px;"></div>
+</body>
 <script type="text/javascript" src="<?=$PUBLIC?>/js/jquery-1.8.1.min.js"></script>
 <!-- 引入BUI -->
 <script type="text/javascript" src="<?=$PUBLIC?>/js/bui-min.js"></script>
 <script type="text/javascript" src="<?=$PUBLIC?>/js/config-min.js"></script>
-</head>
-
-<body>
-</body>
 <script type="text/javascript">
 	//渲染配置列表数据
 	BUI.use(['bui/grid'],function(Grid){
@@ -18,22 +21,15 @@
 							return '<input type="checkbox"  class="bui-grid-cell-text-checkbox" value="'+obj.id+'"/>';
 						}},
 						{title : 'id',dataIndex :'id', width:'10%'},
-			       		{title : '配置项英文名',dataIndex :'title', width:'20%'},
-			       		{title : '配置项中文名',dataIndex : 'cate_cname',width:'20%'},
-			       		{title : '配置项值',dataIndex :'ctime', width:'20%',renderer:BUI.Grid.Format.datetimeRenderer},
+			       		{title : '配置项英文名',dataIndex :'config_key', width:'20%'},
+			       		{title : '配置项中文名',dataIndex : 'config_cname',width:'20%'},
+			       		{title : '配置项值(1为是 0为否)',dataIndex :'config_value', width:'20%'},
 			       		{title : '操作',dataIndex :'top', width:'10%',
 				       		renderer:function(value,obj){
-					       		 if(value === '1'){
-						       		   return '<a onClick="javascript:topArticle('+obj.id+',0,this)" href="javascript:void(0)">取消置顶</a> <a href="javascript:editArticle('+obj.id+')">编辑</a> <a href="javascript:delArticle('+obj.id+')">删除</a>';
-					       		 } else {
-						       			return '<a onClick="javascript:topArticle('+obj.id+',1,this)" href="javascript:void(0)">置顶</a> <a href="javascript:editArticle('+obj.id+')">编辑</a> <a href="javascript:delArticle('+obj.id+')">删除</a>';
-					       		 }
+						       			return '<a href="javascript:delConfig('+obj.id+')">删除</a>';
 					       	}
 			       		}];
-		var data = <?=$config?>;
-		for(i in data){
-			data[i]['ctime'] = parseInt(data[i]['ctime'])*1000;//BUI.Grid.Format.datetimeRenderer的参数为整形 所以在这里需要对其进行强制类型转换 （并且要注意 是毫秒数）
-		}
+		 var data = <?=$config?>;
 		 var grid = new Grid.SimpleGrid({
 		  render:'#grid',
 		  columns : columns,
@@ -41,12 +37,39 @@
 		 });
  		 grid.render();
 	});
-
-<script type="text/javascript" src="<?=$PUBLIC?>/js/jquery-1.8.1.min.js"></script>
-<!-- 引入BUI -->
-<script type="text/javascript" src="<?=$PUBLIC?>/js/bui-min.js"></script>
-<script type="text/javascript" src="<?=$PUBLIC?>/js/config-min.js"></script>
-<script type="text/javascript">
-
+	
+	function delConfig(id){
+		BUI.Message.Confirm('确定要删除该项吗?',function(){
+			  	$.ajax({
+					'url':'ajaxDelConfigs',
+					'type':'POST',
+					'data':{'article_ids':id},
+					'dataType':'json',
+					'success':function(data){
+						if(data){
+							 BUI.Message.Show({
+								msg : '删除成功',
+								icon : 'success',
+								buttons : [],
+								autoHide : true,
+								autoHideDelay : 1000
+							 });
+						} else {
+							 BUI.Message.Show({
+								msg : '删除失败',
+								icon : 'error',
+								buttons : [],
+								autoHide : true,
+								autoHideDelay : 1000
+							});
+						}
+						function refresh_(){
+							window.location.reload();	
+						}
+						setTimeout(refresh_,1000);
+					}
+				});
+			},'question');
+	}
 </script>
 </html>
