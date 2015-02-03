be0fdb01ee47c69b2f8d91074163bede4215b038
waldenpond <yongbaolinux@gmail.com>
2015-02-03 09:26:24 +0800

    123

--- bui/BeyondCms/admin/controllers/cate.php
+++ bui/BeyondCms/admin/controllers/cate.php
@@ -3,12 +3,12 @@
 class Cate extends CI_Controller{
     public function __construct(){
         parent::__construct();
+        $this->load->database();                                    //连接数据库
         $this->load->library('session_');                           //载入会话函数库
         $this->load->helper('url');
         $this->load->helper('common_func');
         $this->admin_res = base_url().'admin_res/';                 //后台资源存放路径(js css images)
         $this->cur_controller = base_url().'system.php/Content';    //当前控制器路径
-        $this->load->database();                                    //连接数据库
         if(!$this->session_->getSession('admin.login')){
             redirect('admin/login','location');
         }
@@ -18,7 +18,9 @@ class Cate extends CI_Controller{
      * 分类列表
      */
     public function catesList(){
-        $this->load->view('catesList',array('PUBLIC'=>$this->admin_res));
+        $categories = json_encode($this->db->query("SELECT * FROM `bd_category`")->result_array());
+        
+        $this->load->view('catesList',array('PUBLIC'=>$this->admin_res,'categories'=>$categories));
     }
     
     /**
--- bui/BeyondCms/admin/views/catesList.php
+++ bui/BeyondCms/admin/views/catesList.php
@@ -10,31 +10,28 @@
 </head>
 
 <body>
-	<div id="t1">
+	<div id="category_container" style="margin: 20px;">
     </div>
-	
 </body>
 <script type="text/javascript" src="<?=$PUBLIC?>/js/jquery-1.8.1.min.js"></script>
-<script type="text/javascript" src="<?=$PUBLIC?>/js/bui-min.js"></script>
-<script type="text/javascript" src="<?=$PUBLIC?>/js/config-min.js"></script>
+<script type="text/javascript" src="<?=$PUBLIC?>/js/sea.js"></script>
+<script type="text/javascript" src="<?=$PUBLIC?>/js/config-min2.js"></script>
 <script type="text/javascript">
 
 	BUI.use(['bui/tree','bui/menu'],function(Tree,Menu){
-		var data = [
-            {text : '1',id : '1',children: [{text : '11',id : '11'}]},
-            {text : '2',id : '2',expanded : true,children : [
-            {text : '21',id : '21',children : [{text : '211',id : '211'},{text : '212',id : '212'}]},
-            {text : '22',id : '22'}
-            ]},
-            {text : '3',id : '3'},
-            {text : '4',id : '4'}
-        ];
+		var data = [ {text : '1',id : '1',children: [{text : '11',id : '11'}]},
+{text : '2',id : '2',expanded : true,children : [
+{text : '21',id : '21',children : [{text : '211',id : '211'},{text : '212',id : '212'}]},
+{text : '22',id : '22'}
+]},
+{text : '3',id : '3'},
+{text : '4',id : '4'}];
 		 //由于这个树，不显示根节点，所以可以不指定根节点
 		var tree = new Tree.TreeList({
-			  render : '#t1',
+			  render : '#category_container',
 			  root : { //由于要显示根节点，所以需要配置根节点
 				  id : '0',
-				  text : '根节点',
+				  text : '分类根节点',
 				  expanded : true,
 				  children : data
 			  },
@@ -42,6 +39,48 @@
 			  showRoot : true
 		});
 		tree.render();
+	    //构造菜单对象
+	    var menu = new Menu.ContextMenu({
+	    	 children : [
+                {
+                    iconCls:' icon-plus',
+                    text : '新建',
+                    listeners:{
+                        'click':addCategory                  //绑定菜单点击事件
+                    }
+                },
+                {xclass:'menu-item-sparator'},
+                {
+                    iconCls:'icon-remove',
+                    text: '删除'
+                },
+                {
+                    iconCls:'icon-pencil',
+                    text : '编辑',
+                    listeners:{
+                        'click':editCategory
+                    }
+                }
+             ]
+		});
+		
+	    //添加分类
+		function addCategory(e){
+			//console.log(e.target);
+		}
+		//编辑分类
+		function editCategory(){
+			  	
+		}
+	    //tree绑定菜单
+		tree.on('itemcontextmenu',function(e){
+			var item = e.item;
+			console.log(item);
+			tree.setSelected(item);
+			menu.set('xy',[e.pageX,e.pageY]);
+		    menu.show();
+		    return false;
+		});
 		
 	});
 </script>
